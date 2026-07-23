f5a544e50229deddbc83ac3abd22195fefa2a6dc
Kshitiz Tiwari <kingkshitiz05@gmail.com>
2026-07-23 09:48:05 +0530

    .

--- client/src/components/AuthModel.jsx
+++ client/src/components/AuthModel.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 import { FaTimes } from "react-icons/fa";
 import { motion } from "motion/react";
 import { createPortal } from 'react-dom';
-import Auth from '../pages/auth';
+import Auth from '../pages/Auth';
 
 function AuthModel({onClose}) {
     const {userData} = useSelector((state)=>state.user)
